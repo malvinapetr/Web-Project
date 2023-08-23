1275f61de01a2e6128733ff60d736d88301621b0
Μαλβίνα <127251691+malvinapetr@users.noreply.github.com>
2023-08-23 16:34:11 +0300

    The files for product review

--- User Main Menu/Review.php
+++ User Main Menu/Review.php
@@ -1,7 +1,9 @@
 <?php
+    session_start();
+ 
     if (isset($_GET['functionToCall']) && function_exists($_GET['functionToCall'])) {
-        call_user_func($_GET['functionToCall']);
-      }
+          call_user_func($_GET['functionToCall']);
+        }
     
    
    function getPoiOffers(){
@@ -23,9 +25,6 @@
         <th>Πλήρωση κριτηρίων</th>
         <th>Ημερομηνία καταχώρησης</th>
         <th>Απόθεμα</th>
-        <th>Ενημέρωση αποθέματος</th>
-        <th></th>
-        <th></th>
         <th id=moreless>Περισσότερα</th>
         </tr>";
       
@@ -41,13 +40,6 @@
       
           echo "<td>" . $row['sub_date'] . "</td>";
           echo "<td>" . $row['stock'] . "</td>";
-          echo "<td id=stockupd><i class='fa fa-paste'></i></td>";
-          if($row['stock'] == 'όχι'){
-            echo "<td class=nostockthumbdown><i class='fa fa-thumbs-down'></i></td>";
-            echo "<td class=nostockthumbup><i class='fa fa-thumbs-up'></i></td>";} 
-          else if($row['stock'] == 'ναι'){
-            echo "<td class=thumbdown><i class='fa fa-thumbs-down'></i></td>";
-            echo "<td class=thumbup><i class='fa fa-thumbs-up'></i></td>";}
           echo "<td class=more_btn>&#x25BC</td>";
           echo "</tr>";
           }
@@ -73,18 +65,143 @@
           echo "<td><img id=prodimg src='".$row['url']."' alt='Φωτογραφία προϊόντος'></td>";
           echo "<td>" . $row['username'] . "</td>";
           echo "<td>" . $row['t_score'] . "</td>";
-          // echo "<td id=stockupd><i class='fa fa-paste'></i></td>";
-          // if($row['stock'] == 'όχι'){
-          //   echo "<td class=nostockthumbdown><i class='fa fa-thumbs-down'></i></td>";
-          //   echo "<td class=nostockthumbup><i class='fa fa-thumbs-up'></i></td>";} 
-          // else if($row['stock'] == 'ναι'){
-          //   echo "<td class=thumbdown><i class='fa fa-thumbs-down'></i></td>";
-          //   echo "<td class=thumbup><i class='fa fa-thumbs-up'></i></td>";}
+          echo "<td class=stockupd><i class='fa fa-paste' style='font-size:110%;'></i></td>";
+          if($row['stock'] == 'όχι'){
+            echo "<td class=nostockthumbdown><i class='fa fa-thumbs-down' style='font-size:110%;'></i></td>";
+            echo "<td class=nostockthumbup><i class='fa fa-thumbs-up' style='font-size:110%;'></i></td>";} 
+          else if($row['stock'] == 'ναι'){
+            echo "<td class=thumbdown><i class='fa fa-thumbs-down' style='font-size:110%;'></i></td>";
+            echo "<td class=thumbup><i class='fa fa-thumbs-up' style='font-size:110%;'></i></td>";}
         }
         echo "</tr>";
         
       mysqli_close($con);
     }
+
+ 
+    function updateOfferLikes()  
+      {
+      $offer_info = json_decode(file_get_contents("php://input"), true);
+      $offer_id = $offer_info['offerid'];
+      $offer_likes = $offer_info['likescount'];
+      $click_type = $offer_info['typeclick'];
+      
+      $con = mysqli_connect('localhost','root','');
+      mysqli_select_db($con,"ekatanalotis");
+
+      $sql = "UPDATE offers SET lcount='" .$offer_likes."' WHERE id='".$offer_id."' ";
+      mysqli_query($con, $sql);
+
+      $sql="SELECT lcount FROM offers WHERE id = $offer_id"; 
+      $result = mysqli_query($con,$sql);
+        
+      while($row = mysqli_fetch_array($result)) {
+          echo $row['lcount'];
+        }
+
+      $sql="INSERT INTO ldhistory (id,username, type, offer_id,datetime) VALUES (null,'".$_SESSION['username']."',
+      '".$click_type."','".$offer_id."','".date('Y-m-d H:i:s')."')";   
+      mysqli_query($con, $sql);   
+
+      mysqli_close($con);
+    }
+
+
+    function updateOfferDislikes()  
+    {
+      $offer_info = json_decode(file_get_contents("php://input"), true);
+      $offer_id = $offer_info['offerid'];
+      $offer_dislikes = $offer_info['dislikescount'];
+      $click_type = $offer_info['typeclick'];
+      
+      $con = mysqli_connect('localhost','root','');
+      mysqli_select_db($con,"ekatanalotis");
+
+      $sql = "UPDATE offers SET dcount='" .$offer_dislikes."' WHERE id='".$offer_id."' ";
+      mysqli_query($con, $sql);
+
+      $sql="SELECT dcount FROM offers WHERE id = $offer_id"; 
+      $result = mysqli_query($con,$sql);
+      
+      while($row = mysqli_fetch_array($result)) {
+          echo $row['dcount'];}
+
+      $sql="INSERT INTO ldhistory (id,username, type, offer_id,datetime) VALUES (null,'".$_SESSION['username']."',
+      '".$click_type."','".$offer_id."','".date('Y-m-d H:i:s')."')";   
+      mysqli_query($con, $sql);    
+
+    mysqli_close($con);
+  }
+
+
+  function checkLikeStatus()  
+   {
+      $offer_id = file_get_contents("php://input");
+      
+      $con = mysqli_connect('localhost','root','');
+      mysqli_select_db($con,"ekatanalotis");
+
+      $username = strval($_SESSION['username']);
+      $sql= "SELECT count(*) FROM ldhistory WHERE offer_id ='".$offer_id."' and type like 'Προσθήκη like' or type like 'Αφαίρεση like' 
+      and username= '".$username."'"; 
+      $likescnt = mysqli_query($con,$sql);
+
+      while($row = mysqli_fetch_array($likescnt)) {
+        if($row[0] % 2 == 0) echo 0;
+        else echo 1;
+        }
+      
+      mysqli_query($con, $sql);    
+
+    mysqli_close($con);
+  }
+
+
+  function checkDislikeStatus()  
+  {
+     $offer_id = file_get_contents("php://input");
+     
+     $con = mysqli_connect('localhost','root','');
+     mysqli_select_db($con,"ekatanalotis");
+
+     $username = strval($_SESSION['username']);
+     $sql= "SELECT count(*) FROM ldhistory WHERE offer_id ='".$offer_id."' and type like 'Προσθήκη dislike' or type like 'Αφαίρεση dislike' 
+     and username= '".$username."'"; 
+     $likescnt = mysqli_query($con,$sql);
+
+     while($row = mysqli_fetch_array($likescnt)) {
+       if($row[0] % 2 == 0) echo 0;
+       else echo 1;
+       }
+     
+     mysqli_query($con, $sql);    
+
+   mysqli_close($con);
+  }
+
+
+ function updateStock()  
+  {
+    $offer_id = file_get_contents("php://input");
+    
+    $con = mysqli_connect('localhost','root','');
+    mysqli_select_db($con,"ekatanalotis");
+
+    $sql="SELECT stock FROM offers WHERE id = $offer_id"; 
+    $result = mysqli_query($con,$sql);
+    $new_stock;
+      
+    while($row = mysqli_fetch_array($result)) {
+        if ($row['stock'] == 'όχι') $new_stock = 'ναι';
+        else if ($row['stock'] == 'ναι') $new_stock = 'όχι';
+      }
+
+    $sql = "UPDATE offers SET stock='" .$new_stock."' WHERE id='".$offer_id."' ";
+    mysqli_query($con, $sql);  
+    echo $new_stock;
+
+  mysqli_close($con);
+  }
       
 
     
